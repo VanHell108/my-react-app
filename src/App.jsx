e2b5ac1af1be7f53b25d13d86df125e7395e1064
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2025-04-08 21:37:22 +0300

    Moved Card components in another component called Cards, and changed some of their styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
-import Card from './Card'
-import myPic from "./assets/profile_pic.jpg"
-import rocketRaccoonPic from "./assets/rocket_raccoon_profile_pic.jpg"
-import babyGrootPic from "./assets/baby_groot_profile_pic.jpg"
+import Cards from './Cards';
 import ChuckNorrisJokes from './ChuckNorrisJokes';
 import ToDoList from './ToDoList';
 
@@ -11,14 +8,7 @@ function App() {
   return(
     <>
       <Header />
-      <main>
-      <div className="card-container">
-         <Card profilePic={myPic} name="Ivan Semchev" like="playing puzzle video games" asWell="watching Fantasy and Sci-Fi movies"/>
-         <Card profilePic={rocketRaccoonPic} name="Rocket Raccoon" like="building advanced weapons and gadgets" asWell="stealing and scavenging"/>   
-         <Card profilePic={babyGrootPic} name="Baby Groot" like={`"I am Groot"`} asWell={`"We are Groot"`}/>
-         <Card />   
-      </div>
-      </main>
+      <Cards />
       <ToDoList />
       <ChuckNorrisJokes />
       <Footer />
---
+++ src/Cards.jsx
@@ -0,0 +1,18 @@
+import Card from './Card';
+import myPic from "./assets/profile_pic.jpg"
+import rocketRaccoonPic from "./assets/rocket_raccoon_profile_pic.jpg"
+import babyGrootPic from "./assets/baby_groot_profile_pic.jpg"
+
+function Cards() {
+
+   return(
+      <div className="card-container">
+         <Card profilePic={myPic} name="Ivan Semchev" like="playing puzzle video games" asWell="watching Fantasy and Sci-Fi movies"/>
+         <Card profilePic={rocketRaccoonPic} name="Rocket Raccoon" like="building advanced weapons and gadgets" asWell="stealing and scavenging"/>   
+         <Card profilePic={babyGrootPic} name="Baby Groot" like={`"I am Groot"`} asWell={`"We are Groot"`}/>
+         <Card />
+      </div>
+   )
+}
+
+export default Cards;
