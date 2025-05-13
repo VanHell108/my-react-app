247599d5e45b6ce3183508f2b4edd2b964393e62
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2025-05-13 20:45:30 +0300

    Small screen device responsive and some bug fixes

--- src/ChuckNorrisJokes.jsx
+++ src/ChuckNorrisJokes.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const ChuckNorrisJokes = () =>  {
+const ChuckNorrisJokes = () => {
    const [joke, setJoke] = useState('');
+   const [loading, setLoading] = useState(false);
+   const buttonRef = useRef(null);
 
    function fetchJoke() {
+      setLoading(true);
       fetch('https://api.chucknorris.io/jokes/random')
          .then(res => res.json())
-         .then(data => setJoke(data.value))
-         .catch(err => console.log(err));
+         .then(data => {
+            setJoke(data.value);
+            setLoading(false);
+         })
+         .catch(err => {
+            console.log(err);
+            setLoading(false);
+         });
    }
 
-   React.useEffect(() => {
+   useEffect(() => {
       fetchJoke();
    }, []);
-   
+
+   function handleClick() {
+      fetchJoke();
+      if (buttonRef.current) {
+         buttonRef.current.blur();
+      }
+   }
+
    return (
       <div className='chuck-norris-jokes'>
          <h1>Chuck Norris Jokes</h1>
          <p id="joke">{joke || 'Loading...'}</p>
-         <button onClick={fetchJoke}>Get New Joke</button>
+         <button 
+            ref={buttonRef} 
+            onClick={handleClick} 
+            disabled={loading}
+         >
+            {loading ? 'Loading...' : 'Get New Joke'}
+         </button>
       </div>
    );
-}
+};
 
 export default ChuckNorrisJokes;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,42 +1,59 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
-   const location = useLocation();
-   const isContactPage = location.pathname === '/my-react-app/contact';
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+  const isContactPage = location.pathname === '/my-react-app/contact';
 
-   const handleHomeClick = () => {
-      window.scrollTo(0, 0);
-    };
+  const handleHomeClick = () => {
+    window.scrollTo(0, 0);
+    setMenuOpen(false);
+  };
+
+  const handleNavClick = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <header>
       <h1>
-         <Link to="/my-react-app"
-               onClick={handleHomeClick}
-               className='header-link'>
-                  My React Website
-         </Link>
+        <Link
+          to="/my-react-app"
+          onClick={handleHomeClick}
+          className="header-link"
+        >
+          My React Website
+        </Link>
       </h1>
-      <nav>
-         <ul>
-            <li>
-               <Link to="/my-react-app"
-                     onClick={handleHomeClick}>
-                        Home
-               </Link>
-            </li>
-            {!isContactPage && (
-               <>
-                  <li><a href="#cards">Cards</a></li>
-                  <li><a href="#todo">To-Do</a></li>
-                  <li><a href="#jokes">Jokes</a></li>
-               </>
-            )}
-            <li><Link to="/my-react-app/contact">Contact</Link></li>
-         </ul>
+
+      <button
+        className="hamburger"
+        onClick={() => setMenuOpen(prev => !prev)}
+        aria-label="Toggle menu"
+      >
+        ☰
+      </button>
+
+      <nav className={menuOpen ? 'open' : ''}>
+        <ul>
+          <li>
+            <Link to="/my-react-app" onClick={handleHomeClick}>Home</Link>
+          </li>
+          {!isContactPage && (
+            <>
+              <li><a href="#cards" onClick={handleNavClick}>Cards</a></li>
+              <li><a href="#todo" onClick={handleNavClick}>To-Do</a></li>
+              <li><a href="#jokes" onClick={handleNavClick}>Jokes</a></li>
+            </>
+          )}
+          <li>
+            <Link to="/my-react-app/contact" onClick={handleNavClick}>Contact</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
